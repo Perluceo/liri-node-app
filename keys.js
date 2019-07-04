17222c6aa6d1967dceaf5530bc6a368000059ba8
Perluceo <FullStackDevSMU@gmail.com>
2019-07-04 12:54:21 -0500

    git

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('all items are loaded');
+
+exports.spotify = {
+  
+};
+
+
+exports.bandsInTownAPI = {
+  id: "c64e7377ddf9167af5426b26689ba3f7"
+};
+
+exports.omdb = {
+  id: "trilogy"
+}
+
+// the file will export a module object with the components inside it
