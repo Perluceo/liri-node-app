c06469b57c05b07b08588aab7656197a4b42b7c1
Perluceo <FullStackDevSMU@gmail.com>
2019-07-04 13:38:20 -0500

    secret

--- keys.js
+++ keys.js
@@ -1,16 +1,17 @@
 console.log('all items are loaded');
 
 exports.spotify = {
-  
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
 
 
 exports.bandsInTownAPI = {
-  id: "c64e7377ddf9167af5426b26689ba3f7"
+    id: "c64e7377ddf9167af5426b26689ba3f7"
 };
 
 exports.omdb = {
-  id: "trilogy"
+    id: "trilogy"
 }
 
 // the file will export a module object with the components inside it
